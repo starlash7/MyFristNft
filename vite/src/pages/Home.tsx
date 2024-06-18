9899c63605f05b55a7fa6619cc44abf36e57f0ee
starlash7 <yang92sw@gmail.com>
2024-06-18 17:42:13 +0900

    Home 꾸미기

--- vite/src/pages/Home.tsx
+++ vite/src/pages/Home.tsx
@@ -2,7 +2,12 @@ import { Flex } from "@chakra-ui/react";
 import { FC } from "react";
 
 const Home: FC = () => {
-  return <Flex>Home</Flex>;
+  return (
+    <Flex>
+      {" "}
+      <img src="/images/eth-world.png" alt="Eth Home" />
+    </Flex>
+  );
 };
 
 export default Home;
