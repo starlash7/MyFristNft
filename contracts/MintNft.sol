f3b0bdaf73b866035349f0248ceee26112e3bc8b
starlash7 <yang92sw@gmail.com>
2024-06-18 10:54:47 +0900

    mint finish

--- contracts/MintNft.sol
+++ contracts/MintNft.sol
@@ -2,37 +2,30 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract MintNft is ERC721Enumerable, Ownable {
-    string metadataUri;
-    string unrevealedUri;
+contract MintNft is ERC721Enumerable {
+   string metadataUri;
+   
 
-    bool isRevealed;
-
-    constructor(string memory _name, string memory _symbol, string memory _metadataUri, string memory _unrevealedUri) ERC721(_name, _symbol) Ownable(msg.sender) {
+   
+   
+       constructor(string memory _name, string memory _symbol, string memory _metadataUri ) ERC721(_name, _symbol){
         metadataUri = _metadataUri;
-        unrevealedUri = _unrevealedUri;
+        
     }
 
-    function mintNft() public onlyOwner {
+    function mintNft() public {
         require(totalSupply() < 100, "No more mint.");
 
-        uint tokenId = totalSupply() + 1;
+        uint tokenId = totalSupply()  + 1;
 
         _mint(msg.sender, tokenId);
     }
 
-    function tokenURI(uint _tokenId) public view override returns (string memory) {
-        if(isRevealed) {
+  function tokenURI(uint _tokenId) public view override returns (string memory) {
             return string(abi.encodePacked(metadataUri, Strings.toString(_tokenId), ".json")); 
-        } else {
-            return unrevealedUri;
-        }
     }
 
-    function setIsReveal() public onlyOwner {
-        isRevealed = true;
-    }
+  
 }
